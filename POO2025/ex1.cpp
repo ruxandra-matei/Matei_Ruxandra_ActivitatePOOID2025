a05944c0d4d3b35d96064d4ff782ce725eeb1f10
Ruxandra <ruxandra.arventiev@gmail.com>
2025-11-16 23:45:59 +0200

    restaurant

--- POO2025/ex1.cpp
+++ POO2025/ex1.cpp
@@ -1,5 +1,20 @@
 #include <iostream.h>
+
+using namespace std;
+
+struct Restaurant {
+	char nume[50];
+	char adresa[100];
+	int rating;
+};
+
+void afisareRestaurant(const Restaurant& r) {
+	cout << "Numele restaurantului este: " << r.nume << endl;
+	cout << "Adresa: " << r.adresa << endl;
+	cout << "Rating: " << r.rating << endl;
+}
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	Restaurant r1 = {"La Mama", "Strada Florilor 10", 5};
+	afisareRestaurant(r1);
 	return 0;
 }
